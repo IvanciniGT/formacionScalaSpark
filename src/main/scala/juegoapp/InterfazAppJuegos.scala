68c298de05be0a8d9009e860e297ebe5ca5db6ee
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2023-11-20 12:47:21 +0100

    app juegos

--- src/main/scala/juegoapp/InterfazAppJuegos.scala
+++ src/main/scala/juegoapp/InterfazAppJuegos.scala
@@ -1,6 +1,23 @@
 package com.curso
 package juegoapp
 
+import juegoapp.entity.{EstadoAppJuegos, Jugador, Partida}
+
 trait InterfazAppJuegos {
+  def confirmarSalida(): Boolean = ???
+
+  def mostrarEstadisticas(jugador: Jugador, partidas: List[Partida]): Unit = ???
+
+  def solicitarJuego(getJuegos: List[Juego[_, _, _]]): Juego[_,_,_] = ???
+
+  def solicitarOpcion(LISTADO_OPCIONES: List[String], estadoDeLaAplicacion: EstadoAppJuegos): String = ???
+
+  def confirmaCreacionJugador(nombreJugador: String): Boolean = ???
+
+  def mostrarBienvenidaAlJugador(estadoDeLaAplicacion: EstadoAppJuegos): Unit = ???
+
+  def pedirJugador(): String = ???
+
+  def mostrarBienvenida() = ???
 
 }
--- src/main/scala/juegoapp/JuegoAppFlujo.scala
+++ src/main/scala/juegoapp/JuegoAppFlujo.scala
@@ -1,13 +1,55 @@
 package com.curso
 package juegoapp
 
+import juegoapp.entity.{EstadoAppJuegos, Jugador}
+
 class JuegoAppFlujo (private val interfaz: InterfazAppJuegos,
                      private val repositorioDeJuegos: JuegosRepositorio,
                      private val repositorioDeJugadores: JugadoresRepositorio,
                      private val repositorioDePartidas: PartidasRepositorio){
 
-  def iniciarAppJuego(): Unit = {
+  private val LISTADO_OPCIONES = List[Tuple2[String, (EstadoAppJuegos)=>{}]](
+                            ("Jugar a un juego", jugar),
+                            ("Estadísticas", estadisticas),
+                            ("Salir", salir)
+                          );
+  val estadoDeLaAplicacion = new EstadoAppJuegos();
 
+  def iniciarAppJuego(): Unit = {
+    interfaz.mostrarBienvenida();
+
+    while(true) {
+      if(estadoDeLaAplicacion.jugador == null) {
+        val nombreJugador = interfaz.pedirJugador();
+        if (repositorioDeJugadores.existe(nombreJugador)) {
+          estadoDeLaAplicacion.jugador = repositorioDeJugadores.getJugador(nombreJugador).get;
+          interfaz.mostrarBienvenidaAlJugador(estadoDeLaAplicacion)
+        } else {
+          if (interfaz.confirmaCreacionJugador(nombreJugador))
+            estadoDeLaAplicacion.jugador = repositorioDeJugadores.altaJugador(nombreJugador)
+        }
+      }else {
+        // Llegados a este punto, tendría jugador
+        // Mostrar un menu con las opciones: JUGAR, ESTADISTICAS, SALIR
+        val opcion = interfaz.solicitarOpcion(LISTADO_OPCIONES, estadoDeLaAplicacion)
+        // Ejecutamos la opción indicada
+        LISTADO_OPCIONES.filter(opcion => opcion._1 == opcion).head._2(estadoDeLaAplicacion)
+      }
+    }
+  }
+
+  def jugar(estado: EstadoAppJuegos): Unit ={
+    val juego = interfaz.solicitarJuego(repositorioDeJuegos.getJuegos)
+    val partida = juego.jugarPartida(estado.jugador)
+    repositorioDePartidas.altaPartida(partida)
+  }
+  def estadisticas(estado: EstadoAppJuegos): Unit ={
+    val partidas = repositorioDePartidas.getPartidas(estado.jugador)
+    interfaz.mostrarEstadisticas(estado.jugador, partidas)
+  }
+  def salir(estado: EstadoAppJuegos): Unit ={
+    if(interfaz.confirmarSalida())
+      System.exit(0)
   }
 
 }
