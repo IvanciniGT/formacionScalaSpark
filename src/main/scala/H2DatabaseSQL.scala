e24c932cefd7af05c8b4cb2a7fa6ce6b159d84cb
ivanciniGT <ivan.osuna.ayuste@gmail.com>
2023-11-22 12:08:00 +0100

    app juegos

--- src/main/scala/H2DatabaseSQL.scala
+++ src/main/scala/H2DatabaseSQL.scala
@@ -4,36 +4,23 @@ import java.sql.{Connection, DriverManager, ResultSet}
 
 object H2DatabaseSQL {
   def main(args: Array[String]): Unit = {
-    // Define the JDBC URL for the H2 database
-    val url = "jdbc:h2:mem:test" // This creates an in-memory database called "test"
-
-    // Create a connection to the H2 database
+    val url = "jdbc:h2:mem:test"
     //Class.forName("org.h2.Driver")
     val connection: Connection = DriverManager.getConnection(url, "sa", "")
-
     try {
-      // Execute SQL queries or commands here
       val statement = connection.createStatement()
-
-      // Example: Create a table
-      statement.execute("CREATE TABLE IF NOT EXISTS my_table (id INT PRIMARY KEY, name VARCHAR(255))")
-
-      // Example: Insert data
-      statement.execute("INSERT INTO my_table VALUES (1, 'John')")
-      statement.execute("INSERT INTO my_table VALUES (2, 'Alice')")
-
-      // Example: Query data
-      val resultSet: ResultSet = statement.executeQuery("SELECT * FROM my_table")
+      statement.execute("CREATE TABLE IF NOT EXISTS personas (id INT PRIMARY KEY, nombre VARCHAR(255))")
+      statement.execute("INSERT INTO personas VALUES (1, 'Felipe')")
+      statement.execute("INSERT INTO personas VALUES (2, 'Menchu')")
+      val resultSet: ResultSet = statement.executeQuery("SELECT * FROM personas")
 
       while (resultSet.next()) {
         val id = resultSet.getInt("id")
-        val name = resultSet.getString("name")
-        println(s"ID: $id, Name: $name")
+        val nombre = resultSet.getString("nombre")
+        println(s"ID: $id, nombre: $nombre")
       }
-
       resultSet.close()
     } finally {
-      // Close the database connection
       if (connection != null) {
         connection.close()
       }
